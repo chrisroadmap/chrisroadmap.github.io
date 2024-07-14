18d08d75b07f8c725402ba4a663b66e204bd642e
Chris Smith <chrisroadmap@gmail.com>
2024-07-14 12:46:56 +0100

    add slash to prevent complaints

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://chrisroadmap.github.io',
-    base: 'src/pages',
+    base: '/src/pages',
 });
