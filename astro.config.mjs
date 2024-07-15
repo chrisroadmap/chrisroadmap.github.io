79fa9405603ab4a424418f28b7328e1a7ee7999e
Chris Smith <chrisroadmap@gmail.com>
2024-07-15 18:38:39 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://chrisroadmap.github.io',
+    site: 'https://cjsmith.be',
 });
